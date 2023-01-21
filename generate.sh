695904ed5f902f8665014d92e5d502263547a268
Willian Gonçalves <w.goncalves91@gmail.com>
2023-01-20 22:22:42 -0300

    Delete lightline theme generator

--- generate.sh
+++ generate.sh
@@ -1,12 +1,9 @@
 main_theme_folder="$(pwd)/colors"
-lightline_theme_folder="$(pwd)/autoload/lightline/colorscheme"
 lualine_theme_folder="$(pwd)/lua/lualine/themes"
 
 selected_theme=$(fd '\.yml$' './themes/' -x echo {/.} | fzf;)
 yml_file="./themes/${selected_theme}.yml"
-vim_file="${selected_theme}.vim"
 lua_file="${selected_theme}.lua"
 
 ruby ./main.rb $yml_file
-ruby ./tabline.rb $yml_file > "${lightline_theme_folder}/${vim_file}"
 ruby ./lualine.rb $yml_file > "${lualine_theme_folder}/${lua_file}"
