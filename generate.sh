ecfd8c320ad8d07ff5a5d5f897e993f70e58e0d8
Willian Gonçalves <w.goncalves91@gmail.com>
2023-03-01 23:00:01 -0300

    Remove unused code

--- generate.sh
+++ generate.sh
@@ -1,9 +1,3 @@
-main_theme_folder="$(pwd)/colors"
-lualine_theme_folder="$(pwd)/lua/lualine/themes"
-
 selected_theme=$(fd '\.yml$' './themes/' -x echo {/.} | fzf;)
 yml_file="./themes/${selected_theme}.yml"
-lua_file="${selected_theme}.lua"
-
 ruby ./main.rb $yml_file
-ruby ./lualine.rb $yml_file > "${lualine_theme_folder}/${lua_file}"
