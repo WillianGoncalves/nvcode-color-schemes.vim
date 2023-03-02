cc87e1687050c94da76a5d95bb3b122e611f1645
Willian Gonçalves <w.goncalves91@gmail.com>
2023-03-01 21:37:59 -0300

    Handle exceptions

--- main.rb
+++ main.rb
@@ -4,9 +4,14 @@ require_relative './lib/reader'
 require_relative './lib/writers/main_writer'
 require_relative './lib/writers/lualine_writer'
 
-filename = ARGV.first
-reader = Reader.new(ARGF)
-main_config_writer = MainWriter.new(reader, filename)
-main_config_writer.create_file
-lualine_config_writer = LualineWriter.new(reader, filename)
-lualine_config_writer.create_file
+begin
+  filename = ARGV.first
+  reader = Reader.new(ARGF)
+  main_config_writer = MainWriter.new(reader, filename)
+  main_config_writer.create_file
+  lualine_config_writer = LualineWriter.new(reader, filename)
+  lualine_config_writer.create_file
+rescue StandardError => e
+  warn message
+  exit 1
+end
