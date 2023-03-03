b0d805ecfbeca43fbc7aa1c045f5e814eaa94b3d
Willian Gonçalves <w.goncalves91@gmail.com>
2023-03-02 23:36:25 -0300

    Fix main file

--- main.rb
+++ main.rb
@@ -7,11 +7,9 @@ require_relative './lib/writers/lualine_writer'
 begin
   filename = ARGV.first
   reader = Reader.new(ARGF)
-  main_config_writer = MainWriter.new(reader, filename)
-  main_config_writer.create_file
-  lualine_config_writer = LualineWriter.new(reader, filename)
-  lualine_config_writer.create_file
+  MainWriter.new(reader, filename).create_file
+  LualineWriter.new(reader, filename).create_file
 rescue StandardError => e
-  warn message
+  warn e.message
   exit 1
 end
